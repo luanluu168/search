68df5a5094b69b0857730839ef13b9a50a16727e
LuanLuu <lluu3@mail.sfsu.edu>
2021-02-24 23:42:31 -0800

    Wall style & some const colors added

--- public/javascripts/constant.js
+++ public/javascripts/constant.js
@@ -4,6 +4,9 @@ const              MAX_COLS = 20;
 const   END_POSITION_AT_ROW = 5;
 const   END_POSITION_AT_COL = 17;
 const        DEFAULT_WEIGHT = 1;
+const            WALL_COLOR = "rgb(210,105,30)";
+const  START_POSITION_COLOR = "rgb(255,128,128)";
+const    END_POSITION_COLOR = "rgb(255, 0, 0)";
 
 const DIRECTION = [[-1,0], [1,0], [0,1], [0,-1]];
 
