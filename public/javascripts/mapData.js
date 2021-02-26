a91493168dcf78ef5f808c7103d9ba6a7db85b4c
LuanLuu <lluu3@mail.sfsu.edu>
2021-02-25 21:48:59 -0800

    Modify MapData for creating maps

--- public/javascripts/mapData.js
+++ public/javascripts/mapData.js
@@ -1,22 +1,29 @@
-const MAP_1 = {
-    '0': [],
-    '1': [],
-    '2': ['.', '.', '.', '.', '.', '.', '.', 'w'],
-    '3': [],
-    '4': [],
-    '5': [],
-    '6': [],
-    '7': [],
-    '8': [],
-    '9': [],
-    '10': [],
-    '11': [],
-    '12': [],
-    '13': [],
-    '14': [],
-    '15': [],
-    '16': [],
-    '17': [],
-    '18': [],
-    '19': [],
-};
+const MapData = (function () {
+    let instance;
+    let map;
+    let keys;
+
+    function setupKeys() {
+        keys = {};
+        keys['L_MAP']     = MAP.L_MAP;
+        keys['MAZE_MAP']  = MAP.MAZE_MAP;
+        keys['EMPTY_MAP'] = MAP.EMPTY_MAP;
+    };
+
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = this;
+                setupKeys();
+            }
+            return instance;
+        },
+        isWall: function(row, column) {
+            return map[row][column] === 'w';
+        },
+        change: function(newMap) {
+            if(newMap) map = keys[newMap]
+            else map = keys['L_MAP'];
+        }
+    };
+})();
